6ed3b2f50fdef8de8e8d206be5c729a969a10feb
Alvaro Gamez Chavez <gaositd@gmail.com>
2021-08-10 17:18:49 -0500

    Todos los archivos del curso practico de JS

---
+++ 04AnalisisSalarial/js/001salarios.js
@@ -0,0 +1,78 @@
+const mexico =  [];
+mexico.push({
+    name:"Daniela",
+    salary: 15000
+});
+mexico.push({
+    name:"Ayde",
+    salary: 10000
+});
+mexico.push({
+    name:"Alvaro",
+    salary: 14000
+});
+mexico.push({
+    name:"Mauricio",
+    salary: 16000
+});
+mexico.push({
+    name:"Iveth",
+    salary: 19000
+});
+mexico.push({
+    name:"Cristinas",
+    salary: 18000
+});
+mexico.push({
+    name:"Jose",
+    salary: 17000
+});
+mexico.push({
+    name:"Tally",
+    salary: 11000
+});
+mexico.push({
+    name:"Maria",
+    salary: 15000
+});
+mexico.push({
+    name:"Melissa",
+    salary: 12000
+});
+mexico.push({
+    name:"Ana",
+    salary: 13000
+});
+mexico.push({
+    name:"Claudia",
+    salary: 15000
+});
+mexico.push({
+    name:"Jessica",
+    salary: 25000
+});
+mexico.push({
+    name:"Fernando",
+    salary: 20000
+});
+mexico.push({
+    name:"Ale",
+    salary: 21000
+});
+mexico.push({
+    name:"Jorge",
+    salary: 24000
+});
+mexico.push({
+    name:"Ivan",
+    salary: 23000
+});
+mexico.push({
+    name:"Fabian",
+    salary: 22000
+});
+mexico.push({
+    name:"Amilkar",
+    salary: 26000
+});
+console.log(mexico);
